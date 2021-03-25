7a63391859171a9cf0b7f5aaa53d88fa85388d08
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-03-25 14:56:01 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteMetadata: {
+    title: "Pattern Library",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "FTTJ5HxinPis15O_wKANqZxvRgC6XO76E6hKjJr3FPc",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "123",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
