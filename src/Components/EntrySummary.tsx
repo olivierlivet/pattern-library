1491f1c767966ea6088ddb3eac5a7e189564f466
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-04-19 15:11:10 +0200

    Wip pattern corners

--- src/Components/EntrySummary.tsx
+++ src/Components/EntrySummary.tsx
@@ -7,8 +7,35 @@ import {
 } from '@chakra-ui/react'
 import React from 'react'
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 10
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1.3
+    }
+};
+
 const EntrySummary = () => {
-    const Card = () => (
+
+    const Card = ({ index }) => (
         <Box w='240px' h='400px' bg='gray.300'>
             <Image
                 src='https://republiqueduchiffon.com/wp-content/uploads/2021/02/Sewing-pattern-BILLY-6-600x600.jpg'
@@ -19,14 +46,14 @@ const EntrySummary = () => {
             />
             <Stack
                 bg='white'
-                p={ 4 }
-                spacing={ 2 }
+                p={4}
+                spacing={2}
                 h='160px'
 
             >
                 <Text
                     textTransform='uppercase'
-                    fontSize={{ base:'xs', lg:'md' }}
+                    fontSize={{ base: 'xs', lg: 'md' }}
                 >
                     <Text
                         as='span'
@@ -34,30 +61,40 @@ const EntrySummary = () => {
                         display='inline-block'
                         borderBottom='solid 3px'
                         borderBottomColor='#EFCBBF'
-                        pr={ 2 }
+                        pr={2}
                     >Jupe</Text>
                 </Text>
-                <Text>Les 10 plus belles jupes pour l'été →</Text>
+                <Text>#{index} Les 10 plus belles jupes pour l'été →</Text>
             </Stack>
         </Box>
     )
-    return(
-        <Box
-            w='100%'
-            overflowX='scroll'
-            p={4}
+    return (
+        <Box maxW='100vw' position='relative' mx={4}>
+            <Box px={4}>
+            <Carousel
+                responsive={responsive}
 
-        >
-            <HStack
-                w='1300px'
-                spacing='20px'
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                ssr={true} // means to render carousel on server-side.
+                infinite={false}
+                autoPlaySpeed={1000}
+                keyBoardControl={true}
+                customTransition="all .5"
+                transitionDuration={500}
+                containerClass="carousel-container"
+                removeArrowOnDeviceType={["tablet", "mobile"]}
+                deviceType={'superLargeDesktop'}
+                // dotListClass="custom-dot-list-style"
+                itemClass="carousel-item-padding-40-px"
             >
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </HStack>
+                { [1,2,3,4,5,6,7,8,9,10].map( (item, i) => <Card index={i} />)}
+            </Carousel>
+            </Box>
+
+
+            
         </Box>
     )
 }
