6283a3519f21516bee01cb1894aeffca6acb83c2
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-04-20 14:02:14 +0200

    Wip pattern corners

--- src/Components/Nav/Account.tsx
+++ src/Components/Nav/Account.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link as GatsbyLink } from 'gatsby'
+import { Link as GatsbyLink } from 'gatsby'
 
 import {
     Box,
@@ -11,18 +11,28 @@ import {
 import { Router, Link as NavLink, Match } from "@reach/router";
 
 const AccountNav = ({ }) => {
-    return(
+    return (
 
         <Box
             py='2rem'
         >
-            <Center spacing={ 3 }>
+            {/* <Center spacing={ 3 }>
                 <Button mx={2} variant='link' as={ NavLink } to='/fr/'>Retour au site</Button>
                 <Button mx={2} variant='link' as={ NavLink } to='/fr/compte/cart'>Panier</Button>
                 <Button mx={2} variant='link' as={ NavLink } to='/fr/compte/order'>Patrons</Button>
                 <Button mx={2} variant='link' as={ NavLink } to='/fr/compte/contribution'>Avis & Publications</Button>
                 <Button mx={2} variant='link' as={ NavLink } to='/fr/compte/profil'>Profil</Button>
+            </Center> */}
+            <Box maxW='100vw' overflowX='scroll'>
+            <Center spacing={3} w='max-content'>
+                <Button mx={2} as={NavLink} to='/fr/'>Retour au site</Button>
+                <Button mx={2} as={NavLink} to='/fr/compte/cart'>Panier</Button>
+                <Button mx={2} as={NavLink} to='/fr/compte/order'>Patrons</Button>
+                <Button mx={2} as={NavLink} to='/fr/compte/contribution'>Avis & Publications</Button>
+                <Button mx={2} as={NavLink} to='/fr/compte/profil'>Profil</Button>
             </Center>
+            </Box>
+            
         </Box>
     )
 }
