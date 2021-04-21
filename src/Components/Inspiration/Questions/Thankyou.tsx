97c3907c1041b6ffb663a99be88a9c28fd6628ea
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-04-21 11:48:25 +0200

    Wip TPC

--- src/Components/Inspiration/Questions/Thankyou.tsx
+++ src/Components/Inspiration/Questions/Thankyou.tsx
@@ -4,6 +4,7 @@ import {
     Box,
     Button,
     Stack,
+    Link,
     Text
 } from '@chakra-ui/react'
 import { ArrowForwardIcon } from '@chakra-ui/icons'
@@ -13,6 +14,8 @@ const Thankyou = ({ id, index, setStep }) => {
     return (
         <Stack spacing={{ base:4, lg: 6 }}>
            <Text>Un très grand merci, blablabla</Text>
+           <Text>Les plus belles réalisation seront partagées sur le compte @machin, pensez à vous abonner !</Text>
+           <Link as='a' href='https://www.instagram.com/the.patterns.corner'>Instagrame TPC</Link>
             <Box>
                 <Button
                     onClick={() => navigate('/fr')}
