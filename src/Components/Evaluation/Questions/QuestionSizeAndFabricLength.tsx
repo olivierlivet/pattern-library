ccc3d215f4fec1a55c4fb82e54b8244a4d48d723
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-04-22 09:55:37 +0200

    Wip TPC

--- src/Components/Evaluation/Questions/QuestionSizeAndFabricLength.tsx
+++ src/Components/Evaluation/Questions/QuestionSizeAndFabricLength.tsx
@@ -11,15 +11,18 @@ import {
 const QuestionSizeAndFabricLength = ({ id, index, setStep }) => {
     return (
         <>
-            <Text>
+            <Text
+                lineHeight='40px'
+                fontSize='16px'
+            >
                 J'ai cousu
                 {` la Jupe Rita `}
                 en taille{' '}
                 <Box
                     display='inline-block'
                 >
-                    <InputGroup size="sm" w='80px'>
-                        <Input type='number' pattern="[0-9]*" min='20' placeholder="100" />
+                    <InputGroup size="sm" w='60px'>
+                        <Input textAlign='center' fontSize='16px' type='number' pattern="[0-9]*" min='20' placeholder="40" />
                         {/* <InputRightAddon children="cm" /> */}
                     </InputGroup>
                 </Box>
@@ -28,14 +31,14 @@ const QuestionSizeAndFabricLength = ({ id, index, setStep }) => {
                     display='inline-block'
                 >
                     <InputGroup size="sm" w='100px'>
-                        <Input type='number' pattern="[0-9]*" min='20' placeholder="100" />
+                        <Input type='number' fontSize='16px' pattern="[0-9]*" min='20' placeholder="100" />
                         <InputRightAddon children="cm" />
                     </InputGroup>
                 </Box>
                 {' '}de tissu.
                 </Text>
             <Box mt={4}>
-                <Text>Ou alors,  <Button size='sm' variant='link' onClick={() => setStep(3)}>Je ne sais pas</Button></Text>
+                <Text>Ou alors,  <Button size='sm' variant='link' onClick={() => setStep(3)}>je ne sais pas</Button></Text>
             </Box>
         </>
     )
