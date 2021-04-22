883574c466b8e069f23379477aa22b57303da4c6
Olivier Livet <olivierlivet@Mac-mini-de-Olivier.local>
2021-04-22 09:50:09 +0200

    Wip TPC

--- src/Components/Evaluation/Questions/QuestionSizeAndFabricLength.tsx
+++ src/Components/Evaluation/Questions/QuestionSizeAndFabricLength.tsx
@@ -19,7 +19,7 @@ const QuestionSizeAndFabricLength = ({ id, index, setStep }) => {
                     display='inline-block'
                 >
                     <InputGroup size="sm" w='80px'>
-                        <Input type='number' min='20' placeholder="100" />
+                        <Input type='number' pattern="[0-9]*" min='20' placeholder="100" />
                         {/* <InputRightAddon children="cm" /> */}
                     </InputGroup>
                 </Box>
@@ -28,14 +28,14 @@ const QuestionSizeAndFabricLength = ({ id, index, setStep }) => {
                     display='inline-block'
                 >
                     <InputGroup size="sm" w='100px'>
-                        <Input type='number' min='20' placeholder="100" />
+                        <Input type='number' pattern="[0-9]*" min='20' placeholder="100" />
                         <InputRightAddon children="cm" />
                     </InputGroup>
                 </Box>
                 {' '}de tissu.
                 </Text>
             <Box mt={4}>
-                <Button size='sm' variant='outline' onClick={() => setStep(3)}>Je ne sais pas</Button>
+                <Text>Ou alors,  <Button size='sm' variant='link' onClick={() => setStep(3)}>Je ne sais pas</Button></Text>
             </Box>
         </>
     )
